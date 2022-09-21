9b70c43016f8a32f24e7686fb7b387c978dc1c21
jusunjo <jo_ami@naver.com>
2022-09-21 23:25:19 +0900

    fix : 에디터 버그 수정

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -5,23 +5,35 @@ import styled from "styled-components";
 
 const StyledEditor = styled.div``;
 
-const toolbarOptions = [
-    // ["link", "image"],
-    [{ header: [1, 2, 3, false] }],
-    ["bold", "italic", "underline", "strike"],
-    ["blockquote"],
-    [{ list: "ordered" }, { list: "bullet" }],
-    [{ color: [] }, { background: [] }],
-    [{ align: [] }],
+// 옵션에 상응하는 포맷, 추가해주지 않으면 text editor에 적용된 스타일을 볼수 없음
+export const formats = [
+    //'font',
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "align",
+    "color",
+    "background",
 ];
 
-// 옵션에 상응하는 포맷, 추가해주지 않으면 text editor에 적용된 스타일을 볼수 없음
-export const formats = ["header", "font", "size", "bold", "italic", "underline", "strike", "align", "blockquote", "list", "bullet", "indent", "background", "color", "link", "image", "video", "width"];
-
 const modules = {
-    toolbar: {
-        container: toolbarOptions,
-    },
+    toolbar: [
+        //[{ 'font': [] }],
+        [{ header: [1, 2, 3, false] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
+
+        [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
+        ["clean"],
+    ],
 };
 
 const Editor = ({ placeholder, value, setContent }) => {
--- src/page/CreateProject.tsx
+++ src/page/CreateProject.tsx
@@ -2,14 +2,19 @@ import Editor from "../components/Editor";
 import React, { Component, useState } from "react";
 import styled from "styled-components";
 
+const StyledCreateProject = styled.div``;
+
 const CreateProject = () => {
     const [content, setContent] = useState();
 
     console.log(content);
     return (
-        <div>
+        <StyledCreateProject>
+            <div className="aaa">
+                123123<strong>ㅁㄴㅁㄴㅇㅁㄴㅇㅋㅌㅊㅌㅋㅊ</strong>ㅁㄴㅇㅁㄴㅇ
+            </div>
             <Editor setContent={setContent} placeholder={"당신의 프로젝트를 소개해주세요!"} value={content} />
-        </div>
+        </StyledCreateProject>
     );
 };
 
